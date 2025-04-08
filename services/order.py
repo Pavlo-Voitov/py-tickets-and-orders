d18bc3158fe3e9ea2963b13f22fd98c8a9058b62
Pavlo <vojtovp20@gmail.com>
2025-04-08 21:04:01 +0300

    'Solution'

--- services/order.py
+++ services/order.py
@@ -13,11 +13,12 @@ def create_order(
     user = User.objects.get(username=username)
     order = Order.objects.create(user=user)
     if date:
-        order.created_at = datetime.strptime(date, '%Y-%m-%d %H:%M')
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
         order.save()
 
     for ticket_data in tickets:
-        movie_session = MovieSession.objects.get(pk=ticket_data["movie_session"])
+        movie_session = (
+            MovieSession.objects.get(pk=ticket_data["movie_session"]))
         Ticket.objects.create(
             movie_session=movie_session,
             order=order,
